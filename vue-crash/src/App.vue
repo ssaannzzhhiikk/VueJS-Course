89b46ae8d569fe12dd4319097e7a1dfe0da8e0c5
Sanzhar <omarkhanovsanzh@gmail.com>
2025-07-06 00:03:08 +0500

    Using v-for, v-bind, v-if , @click, v-on

--- vue-crash/src/App.vue
+++ vue-crash/src/App.vue
@@ -3,7 +3,20 @@ export default {
   data () {
     return {
       name: "John Doe",
-      status: true,
+      status: 'pending',
+      tasks: ['Task One', 'task Two', 'Task Three'],
+      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if ( this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active';
+      }
     }
   }
 } 
@@ -11,7 +24,20 @@ export default {
 
 <template>
   <h1> {{ name }} </h1>
-  <p v-if="status"> User is active</p>
+  <p v-if="status === 'active'"> User is Active</p>
+  <p v-else-if="status === 'pending'"> User is Pending</p>
+  <p v-else>User is Inactive</p>
+
+  <h2>Tasks:</h2>
+  <ul>
+    <li v-for="task in tasks" :key='task'> {{ task }} </li>
+  </ul>
+  <a v-bind:href="link" target="_blank"> Click </a>
+  <br/>
+
+<!--   <button v-on:click="toggleStatus"> Change Status </button> --> 
+  <button @click="toggleStatus"> Change Status </button>
+
 </template>
 
 <style scoped>
