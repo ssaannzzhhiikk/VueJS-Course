529f6a4861d40b9f6aea3867d4a704558530d9c1
Sanzhar <omarkhanovsanzh@gmail.com>
2025-07-11 01:50:43 +0500

    Added limit to descriptions

--- vue-crash/src/components/JobListings.vue
+++ vue-crash/src/components/JobListings.vue
@@ -5,6 +5,10 @@ import JobListing from '@/components/JobListing.vue';
 
 defineProps({
     limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false,
+    }
 });
 
 const jobs = ref(jobData);
@@ -18,9 +22,17 @@ const jobs = ref(jobData);
                 Browse Jobs
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for='job in jobs' :key="job.id" :job="job"/>
+                <JobListing v-for='job in jobs.slice(0, limit || jobs.length)' :key="job.id" :job="job"/>
             </div>
         </div>
+    </section>
+
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
     </section>
 
 </template>
